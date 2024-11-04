4c2cf40a42bc464af36869276f942da940774ec4
Aminzaya <tab.erd.aminzaya@gmail.com>
2024-11-04 17:19:11 +0800

    first commit

---
+++ backend/todoproject/src/main/java/com/todoapp/todoproject/entity/User.java
@@ -0,0 +1,23 @@
+package com.todoapp.todoproject.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "id", updatable = false, nullable = false)
+    private int id;
+
+    @Column(name = "username", unique = true)
+    private String username;
+    private String password;
+}
